a2c9ea3c438b30d998a48326d541e8426300211a
Vo Dinh Hoang <hoangvo15224@gmail.com>
2026-05-29 10:30:12 +0700

    test du lieu postmain

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -19,6 +19,11 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'role' => \App\Http\Middleware\RoleMiddleware::class,
         ]);
+        $middleware->validateCsrfTokens(except: [
+            'admin/*', 
+            'login',   
+            'logout',  
+        ]);
     })
 
     ->withExceptions(function (Exceptions $exceptions) {
